f19ea5b6c3b6a2803e323525eafedd0baba6ba61
David Garratt <support@commander4j.com>
2024-06-16 19:10:01 +0100

    Update to install4j build config

--- b6SocketTest/src/com/commander4j/network/SocketTest.java
+++ b6SocketTest/src/com/commander4j/network/SocketTest.java
@@ -27,7 +27,7 @@ public class SocketTest extends JFrame {
 
 	public static SocketClientGUI client;
 	public static SocketServerGUI server;
-	public static String version = "5.07";
+	public static String version = "5.08";
 	public static final JImageIconLoader imageIconloader = new JImageIconLoader();
 	public ImageIcon logo = imageIconloader.getImageIcon("logo.gif");
 	public ImageIcon ball = imageIconloader.getImageIcon("ball.gif");
